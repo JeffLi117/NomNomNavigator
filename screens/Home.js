bbc392576c1066dbee455ab5763f4cd25d0cabbc
hanabyu <113151578+hbnnguyen@users.noreply.github.com>
2023-11-18 18:43:07 -0500

    add zip code form to home

--- screens/Home.js
+++ screens/Home.js
@@ -1,30 +1,66 @@
-import React, { useContext } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { AppContext } from '../context/AppContext';
 import FilterPicker from '../components/FilterPicker';
+import ZipCodeForm from '../components/ZipCodeForm'
+import { handleGeocoding, handleNearbySearch } from '../API';
 
 const HomeScreen = ({ navigation }) => {
+  const[zipCode, setZipCode] = useState("");
+  const [places, setPlaces] = useState([]);
   const { toggleSelectedCuisines, toggleSelectedStars, toggleSelectedPrice } = useContext(AppContext);
 
   const handleNavigate = () => {
     navigation.navigate('RestaurantQuickView');
   };
 
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      try {
+        if (zipCode !== "") {
+        const coordinates = await handleGeocoding(zipCode);
+
+          if (coordinates) {
+            const { latitude, longitude } = coordinates;
+            const nearbyPlaces = await handleNearbySearch(latitude, longitude);
+            setPlaces(nearbyPlaces);
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchCoordinates();
+  }, [zipCode]);
+
+  const handleZipCodeSubmit = (zip) => {
+    // Handle the submitted zip code, e.g., show an alert
+    setZipCode(zip)
+  };
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <FilterPicker />
-      <TouchableOpacity
-        style={styles.findButton}
-        onPress={handleNavigate}
-      >
-        <Text style={styles.buttonText}>Go to RestaurantQuickView (Find)</Text>
-      </TouchableOpacity>
-      <Button
-        title="Go to MapScreen"
-        onPress={() => navigation.navigate('MapScreen')}
-      />
-    </View>
+    <ScrollView>
+      <View>
+        <Text>Home Screen</Text>
+        <FilterPicker />
+        <ZipCodeForm onSubmit={handleZipCodeSubmit} />
+        <TouchableOpacity
+          style={styles.findButton}
+          onPress={handleNavigate}
+        >
+          <Text style={styles.buttonText}>Go to RestaurantQuickView (Find)</Text>
+        </TouchableOpacity>
+        <Button
+          title="Go to MapScreen"
+          onPress={() => navigation.navigate('MapScreen')}
+        />
+        <Text>Nearby Places:</Text>
+        {places.map((place) => (
+          <Text key={place.place_id}>{place.name}</Text>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
