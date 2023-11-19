f70339f39fa1d3bf855de423d908aaff26885fcb
JeffLi117 <li.jeffreyt@yahoo.com>
2023-11-19 00:17:07 -0600

    Re-implement price filtering logic and add star filtering logic as well (after query has run); change where fetchCoordinate function is called so that it can run whenever the filtering options have changed (and not just when zip code changes) and the button has been clicked

--- context/AppContext.js
+++ context/AppContext.js
@@ -7,6 +7,14 @@ const AppProvider = ({ children }) => {
     "asian": 0, "japanese": 0, "korean": 0, "american": 0, "breakfast": 0, "diner": 0, "fast food": 0, "italian": 0,
   });
   const [selectedStars, setSelectedStars] = useState(null);
+  const [selectedPrice, setSelectedPrice] = useState([
+    {"text": "$", priceLevel: 0, "toggle": 0},
+    {"text": "$$", priceLevel: 1, "toggle": 0},
+    {"text": "$$$", priceLevel: 2, "toggle": 0},
+    {"text": "$$$$", priceLevel: 3, "toggle": 0},
+    {"text": "$$$$$", priceLevel: 4, "toggle": 0},
+  ]);
+  const [showRange, setShowRange] = useState([]);
   // handleSetLocation makes location an object; ex:
   // {
   //   latitude: someNum,
@@ -25,6 +33,17 @@ const AppProvider = ({ children }) => {
   
   const toggleSelectedStars = (num) => {
     setSelectedStars(num);
+  };
+  
+  const toggleSelectedPrice = (priceLevel) => {
+    setSelectedPrice(prevSelectedPrice => {
+      return prevSelectedPrice.map((element) => {
+        if (element.priceLevel === priceLevel) {
+          return { ...element, toggle: element.toggle === 0 ? 1 : 0 };
+        }
+        return element;
+      });
+    });
   };
 
   const handleSetLocation = (location) => {
@@ -47,8 +66,33 @@ const AppProvider = ({ children }) => {
     setErrorMsg(str);
   };
 
+  useEffect(() => {
+    let counter = 0;
+    let minMaxArr = [];
+    // count how many price levels have toggle values of 1
+    for (let i = 0; i < selectedPrice.length; i++) {
+      if (selectedPrice[i].toggle === 1) {
+        minMaxArr.push(selectedPrice[i].priceLevel)
+        counter += 1;
+      }
+    }
+    if (counter > 1) {
+      let min = Math.min(...minMaxArr);
+      let max = Math.max(...minMaxArr);
+      setShowRange([min, max]);
+    } else if (minMaxArr.length === 1) {
+      setShowRange([minMaxArr[0], minMaxArr[0]]);
+    } else {
+      setShowRange([])
+    }
+  }, [selectedPrice])
+  
+  useEffect(() => {
+    console.log("showRange is ", showRange);
+  }, [showRange])
+
   return (
-    <AppContext.Provider value={{ selectedCuisines, toggleSelectedCuisines, selectedStars, toggleSelectedStars, location, handleSetLocation, errorMsg, handleSetErrorMsg }}>
+    <AppContext.Provider value={{ selectedCuisines, toggleSelectedCuisines, selectedStars, toggleSelectedStars, selectedPrice, toggleSelectedPrice, location, handleSetLocation, errorMsg, handleSetErrorMsg, showRange }}>
       {children}
     </AppContext.Provider>
   );
