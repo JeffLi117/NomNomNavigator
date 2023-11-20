9862470145a65be049965aca146a709fbc08e106
Valentina Valverde <vvgreenville@gmail.com>
2023-11-20 12:32:46 -0500

    Added styling to home page.

--- components/FilterPicker.js
+++ components/FilterPicker.js
@@ -1,85 +1,166 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { AppContext } from "../context/AppContext";
 
 const FilterPicker = ({ navigation }) => {
-  const { selectedCuisines, toggleSelectedCuisines, selectedStars, toggleSelectedStars, selectedPrice, toggleSelectedPrice, showRange } = useContext(AppContext);
+  const {
+    selectedCuisines,
+    toggleSelectedCuisines,
+    selectedStars,
+    toggleSelectedStars,
+    selectedPrice,
+    toggleSelectedPrice,
+    showRange,
+  } = useContext(AppContext);
   const arrOfNums = [1, 2, 3, 4, 5];
-  const arrOfCuisines = ["asian", "italian", "american", "breakfast", "diner", "fast food"]
+  const arrOfCuisines = [
+    "asian",
+    "italian",
+    "american",
+    "breakfast",
+    "diner",
+    "fast food",
+  ];
 
   return (
     <View>
-        <Text>Select the cuisines you're craving!</Text>
-        {arrOfCuisines.map((sel) => {
-            return (
-            <TouchableOpacity
-                key={sel}
-                style={selectedCuisines === sel ? styles.selectedBtn : styles.regularBtn}
-                onPress={() => {
-                    toggleSelectedCuisines(sel);
-                }}
+      <Text style={styles.text}>Select the cuisines you're craving!</Text>
+      {arrOfCuisines.map((sel) => {
+        return (
+          <TouchableOpacity
+            key={sel}
+            style={
+              selectedCuisines === sel ? styles.selectedBtn : styles.regularBtn
+            }
+            onPress={() => {
+              toggleSelectedCuisines(sel);
+            }}
+          >
+            <Text
+              style={
+                selectedCuisines === sel
+                  ? styles.selectedText
+                  : styles.buttonText
+              }
             >
-                <Text style={styles.buttonText}>{sel}</Text>
-            </TouchableOpacity>
-            );
-        })}
-        <Text>What's the minimum star rating you want?</Text>
+              {sel}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+      <Text style={styles.text}>What's the minimum star rating you want?</Text>
+      <View style={styles.container}>
         {arrOfNums.map((num) => {
-            return (
+          return (
             <TouchableOpacity
-                key={num}
-                style={selectedStars === num ? styles.selectedBtn : styles.regularBtn}
-                onPress={() => {
-                    toggleSelectedStars(num);
-                }}
+              key={num}
+              style={
+                selectedStars === num
+                  ? styles.smallSelectedBtn
+                  : styles.smallRegularBtn
+              }
+              onPress={() => {
+                toggleSelectedStars(num);
+              }}
             >
-                <Text style={styles.buttonText}>{num}</Text>
+              <Text
+                style={
+                  selectedStars === num
+                    ? styles.selectedText
+                    : styles.buttonText
+                }
+              >
+                {num}
+              </Text>
             </TouchableOpacity>
-            );
+          );
         })}
-        <Text>What's your price range?</Text>
+      </View>
+      <Text style={styles.text}>What's your price range?</Text>
+      <View style={styles.container}>
         {selectedPrice.map((el) => {
-            return (
+          return (
             <TouchableOpacity
-                key={el.priceLevel}
-                style={el.toggle === 1 ? styles.selectedBtn : styles.regularBtn}
-                onPress={() => {
-                    toggleSelectedPrice(el.priceLevel);
-                }}
+              key={el.priceLevel}
+              style={
+                el.toggle === 1
+                  ? styles.smallSelectedBtn
+                  : styles.smallRegularBtn
+              }
+              onPress={() => {
+                toggleSelectedPrice(el.priceLevel);
+              }}
             >
-                <Text style={styles.buttonText}>{el.text}</Text>
+              <Text
+                style={
+                  el.toggle === 1 ? styles.selectedText : styles.buttonText
+                }
+              >
+                {el.text}
+              </Text>
             </TouchableOpacity>
-            );
+          );
         })}
-        {showRange && showRange.length === 2 ?
-            <TouchableOpacity style={styles.priceRange}>
-                <Text>Min Price Level: {`${showRange[0]}`}</Text>
-                <Text>Max Price Level: {`${showRange[1]}`}</Text>
-            </TouchableOpacity>
-            : null
-        }
+      </View>
+      {showRange && showRange.length === 2 ? (
+        <TouchableOpacity style={styles.priceRange}>
+          <Text>Min Price Level: {`${showRange[0]}`}</Text>
+          <Text>Max Price Level: {`${showRange[1]}`}</Text>
+        </TouchableOpacity>
+      ) : null}
     </View>
   );
 };
 
 export default FilterPicker;
 
-
 const styles = StyleSheet.create({
-    regularBtn: {
-      backgroundColor: 'blue',
-    },
-    selectedBtn: {
-      backgroundColor: 'green',
-    },
-    buttonText: {
-      textAlign: 'center',
-      color: 'white',
-    },
-    priceRange: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        textAlign: 'center',
-    },
-  });
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  regularBtn: {
+    backgroundColor: "#d5bdaf",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+  },
+  selectedBtn: {
+    backgroundColor: "#274690",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+  },
+  smallRegularBtn: {
+    backgroundColor: "#d5bdaf",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    width: "18%",
+  },
+  smallSelectedBtn: {
+    backgroundColor: "#274690",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    width: "18%",
+  },
+  buttonText: {
+    textAlign: "center",
+  },
+  selectedText: {
+    textAlign: "center",
+    color: "white",
+  },
+  priceRange: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    textAlign: "center",
+  },
+  text: {
+    textAlign: "center",
+    paddingTop: 20,
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,40 +1,62 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import { AppContext } from '../context/AppContext';
-import FilterPicker from '../components/FilterPicker';
-import ZipCodeForm from '../components/ZipCodeForm'
-import { handleGeocoding, handleNearbySearch } from '../API';
-import GetExpoLocation from '../components/GetExpoLocation';
+import React, { useContext, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { AppContext } from "../context/AppContext";
+import FilterPicker from "../components/FilterPicker";
+import ZipCodeForm from "../components/ZipCodeForm";
+import { handleGeocoding, handleNearbySearch } from "../API";
+import GetExpoLocation from "../components/GetExpoLocation";
 
 const HomeScreen = ({ navigation }) => {
   const [zipCode, setZipCode] = useState("");
   const [places, setPlaces] = useState([]);
-  const { toggleSelectedCuisines, toggleSelectedStars, toggleSelectedPrice, showRange, selectedStars, selectedCuisine, setCopiedList, copiedList } = useContext(AppContext);
+  const {
+    toggleSelectedCuisines,
+    toggleSelectedStars,
+    toggleSelectedPrice,
+    showRange,
+    selectedStars,
+    selectedCuisine,
+    setCopiedList,
+    copiedList,
+  } = useContext(AppContext);
 
   const handleNavigate = () => {
-    navigation.navigate('RestaurantQuickView');
+    navigation.navigate("RestaurantQuickView");
   };
 
   const filterByPrice = (resultArr) => {
     return resultArr.filter((result) => {
-      return result.price_level >= showRange[0] && result.price_level <= showRange[1];
+      return (
+        result.price_level >= showRange[0] && result.price_level <= showRange[1]
+      );
     });
-  }
+  };
 
   const filterByStars = (resultArr) => {
     return resultArr.filter((result) => {
       return result.rating >= selectedStars;
     });
-  }
+  };
 
   const fetchCoordinates = async () => {
     try {
       if (zipCode !== "") {
-      const coordinates = await handleGeocoding(zipCode);
+        const coordinates = await handleGeocoding(zipCode);
 
         if (coordinates) {
           const { latitude, longitude } = coordinates;
-          const nearbyPlaces = await handleNearbySearch(latitude, longitude, selectedCuisine);
+          const nearbyPlaces = await handleNearbySearch(
+            latitude,
+            longitude,
+            selectedCuisine
+          );
           let filtered;
           let filtersRun = false;
           if (showRange.length === 2) {
@@ -51,27 +73,26 @@ const HomeScreen = ({ navigation }) => {
             }
           }
           if (filtersRun) {
-            setPlaces(filtered); 
+            setPlaces(filtered);
             setCopiedList(filtered);
             console.log("# of filtered results is ", filtered.length);
-          }
-          else {
+          } else {
             setPlaces(nearbyPlaces);
             setCopiedList(nearbyPlaces);
           }
         }
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
   useEffect(() => {
     console.log("copiedList changed");
     if (copiedList.length > 0 && places.length > 0) {
-      navigation.navigate('RestaurantQuickView');
+      navigation.navigate("RestaurantQuickView");
     }
-  }, [copiedList])
+  }, [copiedList]);
 
   const handleZipCodeSubmit = async (zip) => {
     // Handle the submitted zip code, e.g., show an alert
@@ -83,11 +104,10 @@ const HomeScreen = ({ navigation }) => {
   return (
     <ScrollView>
       <View>
-        <Text>Home Screen</Text>
         {/* <GetExpoLocation /> */}
         <FilterPicker />
         <ZipCodeForm onSubmit={handleZipCodeSubmit} />
-        <TouchableOpacity
+        {/* <TouchableOpacity
           style={styles.findButton}
           onPress={handleNavigate}
         >
@@ -96,7 +116,7 @@ const HomeScreen = ({ navigation }) => {
         <Button
           title="Go to MapScreen"
           onPress={() => navigation.navigate('MapScreen')}
-        />
+        /> */}
         <Text>Nearby Places:</Text>
         {/* {places.map((place) => (
           <Text key={place.place_id}>{place.name}</Text>
@@ -108,13 +128,12 @@ const HomeScreen = ({ navigation }) => {
 
 export default HomeScreen;
 
-
 const styles = StyleSheet.create({
   findButton: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   buttonText: {
-    textAlign: 'center',
-    color: 'white',
+    textAlign: "center",
+    color: "white",
   },
 });
